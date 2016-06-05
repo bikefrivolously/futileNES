5c053d225810f1e2cdea9d4a30ef0c1af779ada9
Dan Martins <bikefrivolously@users.noreply.github.com>
2016-06-05 18:44:36 -0400

    Basic logic added to main.rs to read a given NES rom file and begin parsing it with the rom module.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+mod rom;
+
+use std::env;
+use std::path::Path;
+use std::io::Read;
+use std::fs::File;
+
+fn usage() {
+    println!("Usage: futilenes <rom>");
+}
+
+fn read_rom<P: AsRef<Path>>(path: P) -> Vec<u8> {
+    let mut file = File::open(path).unwrap();
+    let mut buf: Vec<u8> = Vec::new();
+    match file.read_to_end(&mut buf) {
+        Ok(bytes) => println!("Read {} bytes from ROM.", bytes),
+        Err(_) => panic!("problem reading from ROM.")
+    }
+    buf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    if args.len() != 2 {
+        usage();
+        return;
+    }
+    let rom_filename = match args.nth(1) {
+        Some(a) => a,
+        None => panic!("problem in argument parsing."),
+    };
+    println!("ROM: {}", rom_filename);
+
+    let rom_data = read_rom(rom_filename);
+    let rom = rom::iNESFile::load(rom_data);
+    rom.info();
+    rom.dump_prg_rom();
 }
